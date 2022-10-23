ecab2f0f0e6a989648db2ac41fb4376cce666190
thecsharper <si@thecsharper.com>
2022-10-23 20:02:39 +0100

    Added test setup

--- MartinDemo.Tests/MartenDemoTests.cs
+++ MartinDemo.Tests/MartenDemoTests.cs
@@ -1,11 +1,27 @@
+using Microsoft.Extensions.Logging;
+
+using Marten;
+using Moq;
+
+using MartenDemo.Controllers;
+
 namespace MartinDemo.Tests
 {
     public class MartenDemoTests
     {
+        private readonly Mock<ILogger<MartenController>> _logger;
+        
+        public MartenDemoTests()
+        {
+            _logger = new Mock<ILogger<MartenController>>();
+        }
+
         [Fact]
         public void Test1()
         {
+            var session = new Mock<IDocumentSession>();
 
+            var controller = new MartenController(_logger.Object);
         }
     }
 }
