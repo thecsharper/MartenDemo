fa99b14351e17d46b75dba09a162f3159a3c793e
thecsharper <si@thecsharper.com>
2022-10-24 21:03:02 +0100

    Updated test - not passing

--- MartinDemo.Tests/MartenDemoTests.cs
+++ MartinDemo.Tests/MartenDemoTests.cs
@@ -1,27 +1,49 @@
 using Microsoft.Extensions.Logging;
 
 using Marten;
+using Marten.Linq;
+
 using Moq;
+using Moq.AutoMock;
 
 using MartenDemo.Controllers;
+using MartenDemo.Models;
 
 namespace MartinDemo.Tests
 {
     public class MartenDemoTests
     {
         private readonly Mock<ILogger<MartenController>> _logger;
-        
+
         public MartenDemoTests()
         {
             _logger = new Mock<ILogger<MartenController>>();
         }
 
         [Fact]
-        public void Test1()
+        public void Marten_Controller_Stream()
         {
             var session = new Mock<IDocumentSession>();
 
-            var controller = new MartenController(_logger.Object);
+            var martenInput = new MartenData()
+            {
+                Id = Guid.NewGuid(),
+                Date = DateTime.UtcNow
+            };
+
+            var testData = new[]
+            {
+               martenInput
+            }.AsQueryable();
+
+            var martenData = new Mock<IMartenQueryable<MartenData>>();
+
+            session.Setup(x => x.Query<MartenData>()).Returns(martenData.Object);
+
+            var mocker = new AutoMocker();
+            var controller = mocker.CreateInstance<MartenController>();
+
+            controller.Stream(session.Object, martenInput);
         }
     }
 }
