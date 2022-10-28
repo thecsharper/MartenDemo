16e1886a2443ec4fffeba7fc28942995e7f0865c
thecsharper <si@thecsharper.com>
2022-10-28 22:23:37 +0100

    Updated unit tests

--- MartinDemo.Tests/MartenDemoTests.cs
+++ MartinDemo.Tests/MartenDemoTests.cs
@@ -7,25 +7,20 @@ using Moq.AutoMock;
 using MartenDemo.Controllers;
 using MartenDemo.Models;
 using MartenDemo;
+using System.ComponentModel;
 
 namespace MartinDemo.Tests
 {
+    [Category("Marten Tests")]
     public class MartenDemoTests
     {
         [Fact]
+        [Trait("Category", "Unit")]
         public async Task Marten_Controller_Get()
         {
             var session = new Mock<IDocumentSession>();
 
-
-            var id = Guid.NewGuid();
-            var martenDate = DateTime.UtcNow;
-
-            var martenInput = new MartenData
-            {
-                Id = id,
-                Date = martenDate
-            };
+            var martenInput = GetMartenData();
 
             var mocker = new AutoMocker();
             mocker.Use<IMartenQueries>(mock => mock.GetSingleItem(It.IsAny<Guid>()) == martenInput);
@@ -33,26 +28,54 @@ namespace MartinDemo.Tests
 
             var result = await controller.Get(session.Object, martenInput);
 
-            result.Id.Should().Be(id);
-            result.Date.Should().Be(martenDate);
+            result.Id.Should().Be(martenInput.Id);
+            result.Date.Should().Be(martenInput.Date);
         }
 
         [Fact]
+        [Trait("Category", "Unit")]
+        public async Task Marten_Controller_GetData()
+        {
+            var session = new Mock<IDocumentSession>();
+
+            var martenList = new List<MartenData>();
+
+            var martenInput = GetMartenData();
+
+            martenList.Add(martenInput);
+
+            var mocker = new AutoMocker();
+            mocker.Use<IMartenQueries>(mock => mock.GetManyItems(It.IsAny<Guid>()) == martenList);
+            var controller = mocker.CreateInstance<MartenController>();
+
+            var result = await controller.GetData(session.Object, martenInput);
+
+            result.First().Id.Should().Be(martenInput.Id);
+            result.First().Date.Should().Be(martenInput.Date);
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
         public void Marten_Controller_Stream()
         {
             var session = new Mock<IDocumentSession>();
 
-            var martenInput = new MartenData
-            {
-                Id = Guid.NewGuid(),
-                Date = DateTime.UtcNow
-            };
+            var martenInput = GetMartenData();
 
             var mocker = new AutoMocker();
             mocker.Use<IMartenQueries>(mock => mock.GetSingleItem(It.IsAny<Guid>()) == martenInput);
             var controller = mocker.CreateInstance<MartenController>();
             
             controller.Stream(session.Object, martenInput);
+        }
+
+        private static MartenData GetMartenData()
+        {
+            return new MartenData
+            {
+                Id = Guid.NewGuid(),
+                Date = DateTime.UtcNow
+            };
         }
     }
 }
