ce2cf29d1bd6a391136670d504aa03c0ad0b92e8
thecsharper <si@thecsharper.com>
2022-11-09 16:39:04 +0000

    Test setup

--- MartinDemo.AcceptanceTests/ApiTests.cs
+++ MartinDemo.AcceptanceTests/ApiTests.cs
@@ -1,6 +1,17 @@
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc.Testing;
+
+using MartenDemo;
+
+using FluentAssertions;
+
 namespace MartinDemo.AcceptanceTests
 {
-    public class ApiTests
+    public class ApiTests : IClassFixture<WebApplicationFactory<Program>>
     {
         [Fact]
         public void Test1()
