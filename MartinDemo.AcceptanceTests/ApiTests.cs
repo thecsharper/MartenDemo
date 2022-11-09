9c87350ac321b1f3c01d8734fbfbf12d53d727a9
thecsharper <si@thecsharper.com>
2022-11-09 16:46:34 +0000

    Initial test setup

--- MartinDemo.AcceptanceTests/ApiTests.cs
+++ MartinDemo.AcceptanceTests/ApiTests.cs
@@ -8,15 +8,33 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using MartenDemo;
 
 using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
 
 namespace MartinDemo.AcceptanceTests
 {
+    // https://learn.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-7.0
     public class ApiTests : IClassFixture<WebApplicationFactory<Program>>
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly WebApplicationFactory<Program> _factory;
 
+        public ApiTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
         }
+
+        [Theory]
+        [InlineData("/search")]
+        public async Task ProductSearchImage_valid_message_returns_global_event_id(string url)
+        {
+            var options = new WebApplicationFactoryClientOptions { AllowAutoRedirect = false };
+            var client = _factory.CreateClient(options);
+
+            var response = await client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Content.Headers.ContentType!.ToString().Should().Be("application/json; charset=utf-8");
+        }
+
     }
 }
