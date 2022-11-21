c16865598d06f1fcfe2faeeedae10ca078411cfd
thecsharper <si@thecsharper.com>
2022-11-21 20:08:10 +0000

    Experimental user class

---
+++ MartenDemo/Helpers/Users.cs
@@ -0,0 +1,28 @@
+﻿namespace MartenDemo.Helpers
+{
+    public class UserRepository
+    {
+        public User Get(string username, string password)
+        {
+            var users = new List<User>()
+            {
+                new User() {
+                             Id = 1,
+                             Username = "test",
+                             Password = "423423rFAsas£!^^34rrsdf",
+                             Role = "user"
+                           }
+            };
+
+
+            var result = users.FirstOrDefault(user => user.Username!.ToLower() == username && user.Password == password);
+            
+            if (result != null)
+            {
+                return result;
+            }
+
+            return new User();
+        }
+    }
+}
